f2f12cfb644f1a4dcdb90314593e32b4b0810237
neloydutta <neloy.dutta@live.com>
2016-12-04 16:54:48 +0530

    FirstCommit

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1480850160,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/background.png",
+		"images/trash-sheet0.png",
+		"images/bin-sheet0.png",
+		"images/gobackground.png",
+		"images/gameover-sheet0.png",
+		"images/replay-sheet0.png",
+		"images/menubackground.png",
+		"images/art-sheet0.png",
+		"images/play-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
